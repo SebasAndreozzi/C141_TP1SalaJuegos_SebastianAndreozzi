e6514839bdea4f684e04313f440629c7f208374c
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2026-05-11 20:22:19 -0300

    Agrego header que permita moverse entre los componentes libremente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Signup } from './components/signup/signup';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'login', component: Login },
+    
+    { path:'signup', component: Signup },
+
+    { path:'', component: Home },
+
+    { path:'about', component: About },
+
+    { path:'**', redirectTo: '' }
+];
---
+++ src/app/components/header/header.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+}
